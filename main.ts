59ab02f0bf8c63f1c24914f88464005b9542894b
vesta-sudo <115300048+vesta-sudo@users.noreply.github.com>
2022-10-07 13:43:22 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,20 @@
+input.onButtonPressed(Button.A, function () {
+    for (let index = 0; index < 100000; index++) {
+    	
+    }
+})
+input.onButtonPressed(Button.AB, function () {
+    for (let index = 0; index < 100000; index++) {
+    	
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    for (let index = 0; index < 100000; index++) {
+    	
+    }
+})
+input.onGesture(Gesture.Shake, function () {
+    for (let index = 0; index < 100000; index++) {
+    	
+    }
+})
